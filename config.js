89a8051f8d49c6ae2d15cf9e018fcde43edd215b
ojuolokun86 <ojuolokun86@gmail.com>
2025-05-22 14:49:10 +0100

    repair view once web

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-const URL = 'https://bmm2-production.up.railway.app';
-//const URL = 'http://localhost:3000'; // Replace with your actual URL
+//const URL = 'https://bmm2-production.up.railway.app';
+const URL = 'http://localhost:3000'; // Replace with your actual URL
 //const URL = 'https://bmm-bot-2.fly.dev';
 
 
